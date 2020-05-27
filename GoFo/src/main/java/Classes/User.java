e624d0d06e928671af418c3b3c71818e7248a9e4
Mahmoud Salah <51229687+MahmoudHendi1@users.noreply.github.com>
2020-05-28 00:12:43 +0200

    Base

---
+++ GoFo/src/main/java/Classes/User.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+//import org.apache.commons.validator.routines.EmailValidator;
+/**
+ *
+ * @author Lenovo
+ */
+public class User {
+    int id;
+    String name,
+           password,
+           email,
+           phoneNumber,
+           address;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+ 
+
+
+    
+}
