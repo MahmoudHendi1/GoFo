4d90fe07470e05b590ba643bd1d5deb2e5e6ed8b
MahmoudHendi <51229687+MahmoudHendi1@users.noreply.github.com>
2020-05-29 04:20:25 +0200

    Edit Main GUI

---
+++ GoFo/src/main/java/Administrators/AdministratorManager.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Administrators;
+
+import Utilits.Playground;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class AdministratorManager {
+    public Boolean approvePlayground(Playground P){
+        return true;
+    }
+    public Boolean suspendPlayground(Playground P){
+        return true;
+    }
+    public void deletePlayground(Playground P){
+        
+    }
+    public void activePlayground(Playground P){
+    }
+    
+    
+}
